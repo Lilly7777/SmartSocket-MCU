cb972a542d20522bc2a7582f146b8b1dd3f18e1b
Lilly Lubenova <lilly.lubenova@gmail.com>
2020-12-20 18:01:30 +0200

    Added MCU state indication template

--- communication/communication.ino
+++ communication/communication.ino
@@ -1,6 +1,10 @@
 #include <ESP8266WiFi.h>
-//#include <ESP8266WebServer.h>
+
 #include <vector>
+
+#define LED_STATE_RED   16    //D0
+#define LED_STATE_GREEN 5   //D1
+#define LED_STATE_BLUE  4    //D2  
 
 #define KNOWN_NETWORKS_MAX 5
 
@@ -15,8 +19,13 @@ WiFiServer server(80);
 
 bool AP_On = (known_networks.size() == 0);  //TODO: Add more cases when AP will be on
 
-void setup() {
+void setup(){
   Serial.begin(9600);
+  
+  pinMode(LED_STATE_RED, OUTPUT);
+  pinMode(LED_STATE_GREEN, OUTPUT);
+  pinMode(LED_STATE_BLUE, OUTPUT);
+  
   if(AP_On){
     WiFi.softAP("SmartSocket");
     server.begin();
@@ -44,4 +53,16 @@ void loop() {
       String html ="<!DOCTYPE html><html> <head> <title>SmartSocket - Welcome</title> <style> .center{ width:250px; height:350px; margin: 0 auto; } </style> </head> <body> <div class=\"center\"> <h2>Welcome!</h2> <h3>Enter your WiFi's credentials</h3> <form method=\"post\"> <label for=\"label_ssid\">SSID:</label><br> <input type=\"text\" id=\"input_ssid\" name=\"input_ssid\"><br> <label for=\"label_pswd\">Password:</label><br> <input type=\"text\" id=\"input_pswd\" name=\"input_pswd\"><br><br> <input type=\"submit\" value=\"Submit\"> </form> </div> </body> </html>";
       client.println(html);
     }
+}
+
+/*
+  Turned off state (RED)       = 255, 0,  0
+  Not connected state (ORANGE) = 255, 20, 0
+  Turned on state (GREEN)      =   0, 45, 0
+*/
+
+void change_state_led(int R_VALUE, int G_VALUE, int B_VALUE){
+  analogWrite(R_VALUE, R_VALUE);
+  analogWrite(G_VALUE, G_VALUE);
+  analogWrite(B_VALUE, B_VALUE);
 }

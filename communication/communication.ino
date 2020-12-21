4c2453a70b63bf7b7e8045e57bb865aaa47d0b18
Lilly Lubenova <lilly.lubenova@gmail.com>
2020-12-21 16:22:36 +0200

    Migrated from WiFiServer to ESP8266WebServer.

--- communication/communication.ino
+++ communication/communication.ino
@@ -1,12 +1,19 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
+
+#include "WebPages.h"
 
 #include <vector>
 
-#define LED_STATE_RED   16    //D0
-#define LED_STATE_GREEN 5   //D1
-#define LED_STATE_BLUE  4    //D2  
+#define LED_STATE_RED        5    // D1
+#define LED_STATE_GREEN      4    // D2
 
-#define KNOWN_NETWORKS_MAX 5
+#define RELAY_COM           14    // D5
+
+#define RESET_BUTTON        13    // D7
+
+#define KNOWN_NETWORKS_MAX   5
 
 typedef struct network_t{
     String ssid;
@@ -15,20 +22,18 @@ typedef struct network_t{
 
 std::vector<Network> known_networks;
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
 
 bool AP_On = (known_networks.size() == 0);  //TODO: Add more cases when AP will be on
 
 void setup(){
   Serial.begin(9600);
-  
-  pinMode(LED_STATE_RED, OUTPUT);
-  pinMode(LED_STATE_GREEN, OUTPUT);
-  pinMode(LED_STATE_BLUE, OUTPUT);
-  
   if(AP_On){
     WiFi.softAP("SmartSocket");
+    server.on("/", rootHandler);
     server.begin();
+    Serial.println("Web server started!");
+
   }else{
     delay(1000); // Wait for one second before trying to connect
     WiFi.softAPdisconnect(true); //Make sure AP is turned off
@@ -43,16 +48,17 @@ void setup(){
   Serial.println("MCU started successfully!");
   Serial.println(WiFi.softAPIP());
   Serial.println(AP_On);
-  Serial.println(is_connected);
   /* end Debug */
 }
 
 void loop() {
     if(AP_On){
-      WiFiClient client = server.available();  
-      String html ="<!DOCTYPE html><html> <head> <title>SmartSocket - Welcome</title> <style> .center{ width:250px; height:350px; margin: 0 auto; } </style> </head> <body> <div class=\"center\"> <h2>Welcome!</h2> <h3>Enter your WiFi's credentials</h3> <form method=\"post\"> <label for=\"label_ssid\">SSID:</label><br> <input type=\"text\" id=\"input_ssid\" name=\"input_ssid\"><br> <label for=\"label_pswd\">Password:</label><br> <input type=\"text\" id=\"input_pswd\" name=\"input_pswd\"><br><br> <input type=\"submit\" value=\"Submit\"> </form> </div> </body> </html>";
-      client.println(html);
+     server.handleClient();
     }
+}
+
+void rootHandler() {
+  server.send(200, "text/html", index_page);
 }
 
 /*

2a41c6738d277c8d72dcfcec164f3033a4c787c3
Lilly Lubenova <lilly.lubenova@gmail.com>
2020-12-21 15:52:44 +0200

    Moved HTML to an extern header.

---
+++ communication/WebPages.h
@@ -0,0 +1,33 @@
+#ifndef WEBPAGES_H
+#define WEBPAGES_H
+const char index_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>SmartSocket - Welcome</title>
+      <style>
+         .center{
+         width:250px; 
+         height:350px; 
+         margin: 0 auto;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="center">
+         <h2>Welcome!</h2>
+         <h3>Enter your WiFi's credentials</h3>
+         <form action="/process" method="get">
+            <label for="label_ssid">SSID:</label><br>
+            <input type="text" id="input_ssid" name="input_ssid"><br>
+            <label for="label_pswd">Password:</label><br>
+            <input type="text" id="input_pswd" name="input_pswd"><br><br>
+            <input type="submit" value="Submit">
+         </form>
+      </div>
+   </body>
+</html>
+
+)=====";
+
+#endif
